01222f769d2f6ad93795954a9138118de26a9a1f
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2015-11-14 21:37:58 +0100

    init project

---
+++ src/main/java/de/yaio/services/plantuml/controller/PlantumlController.java
@@ -0,0 +1,142 @@
+/** 
+ * software for diagram-converter
+ * 
+ * @FeatureDomain                Converter
+ * @author                       Michael Schreiner <michael.schreiner@your-it-fellow.de>
+ * @category                     diagram-services
+ * @copyright                    Copyright (c) 2014, Michael Schreiner
+ * @license                      http://mozilla.org/MPL/2.0/ Mozilla Public License 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.yaio.services.plantuml.controller;
+
+import javax.imageio.IIOException;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sourceforge.plantuml.FileFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/** 
+ * controller with Download-Services to convert plantuml-src to diagrams
+ *  
+ * @FeatureDomain                Webservice
+ * @package                      de.yaio.services.plantuml.controller
+ * @author                       Michael Schreiner <michael.schreiner@your-it-fellow.de>
+ * @category                     diagram-services
+ * @copyright                    Copyright (c) 2014, Michael Schreiner
+ * @license                      http://mozilla.org/MPL/2.0/ Mozilla Public License 2.0
+ */
+@Controller
+@RequestMapping("${plantuml.baseurl}")
+public class PlantumlController {
+
+    @Autowired
+    protected PlantumlUtils converterUtils;
+    
+    /** 
+     * Request to generate plantuml-diagram from src as png
+     * @FeatureDomain                Webservice
+     * @FeatureResult                send diagram-data via HttpServletResponse
+     * @FeatureKeywords              Webservice
+     * @param src                    compressed plantuml-src
+     * @param response               the response-Obj to set contenttype and headers
+     */
+    @RequestMapping(method = RequestMethod.GET, 
+                    value = "/png/{src}",
+                    produces = "image/png")
+    public @ResponseBody void convertToPng(@PathVariable(value="src") String src,
+                                        HttpServletResponse response) {
+        try {
+            // build the UML source from the compressed request parameter
+            
+            String uml = converterUtils.getUmlSource(src);
+            converterUtils.exportAsDiagramm(uml, response, FileFormat.PNG);
+        } catch (IIOException iioe) {
+                // Browser has closed the connection, so the HTTP OutputStream is closed
+                // Silently catch the exception to avoid annoying log
+        } catch (Exception e) {
+            System.err.println("exception start for src:" + src + " ex:" + e);
+            e.printStackTrace();
+        }
+    }
+
+    /** 
+     * Request to generate plantuml-diagram from src as png
+     * @FeatureDomain                Webservice
+     * @FeatureResult                send diagram-data via HttpServletResponse
+     * @FeatureKeywords              Webservice
+     * @param src                    compressed plantuml-src
+     * @param response               the response-Obj to set contenttype and headers
+     */
+    @RequestMapping(method = RequestMethod.GET, 
+                    value = "/img/{src}",
+                    produces = "image/png")
+    public @ResponseBody void convertToImg(@PathVariable(value="src") String src,
+                                        HttpServletResponse response) {
+        this.convertToPng(src, response);
+    }
+
+    /** 
+     * Request to generate plantuml-diagram from src as svg
+     * @FeatureDomain                Webservice
+     * @FeatureResult                send diagram-data via HttpServletResponse
+     * @FeatureKeywords              Webservice
+     * @param src                    compressed plantuml-src
+     * @param response               the response-Obj to set contenttype and headers
+     */
+    @RequestMapping(method = RequestMethod.GET, 
+                    value = "/svg/{src}",
+                    produces = "image/png")
+    public @ResponseBody void convertToSvg(@PathVariable(value="src") String src,
+                                        HttpServletResponse response) {
+        try {
+            // build the UML source from the compressed request parameter
+            
+            String uml = converterUtils.getUmlSource(src);
+            converterUtils.exportAsDiagramm(uml, response, FileFormat.SVG);
+        } catch (IIOException iioe) {
+                // Browser has closed the connection, so the HTTP OutputStream is closed
+                // Silently catch the exception to avoid annoying log
+        } catch (Exception e) {
+            System.err.println("exception start for src:" + src + " ex:" + e);
+            e.printStackTrace();
+        }
+    }
+
+    /** 
+    * Request to generate plantuml-diagram from src as ascii
+    * @FeatureDomain                Webservice
+    * @FeatureResult                send diagram-data via HttpServletResponse
+    * @FeatureKeywords              Webservice
+    * @param src                    compressed plantuml-src
+    * @param response               the response-Obj to set contenttype and headers
+    */
+   @RequestMapping(method = RequestMethod.GET, 
+                   value = "/txt/{src}",
+                   produces = "text/plain")
+   public @ResponseBody void convertToAscii(@PathVariable(value="src") String src,
+                                       HttpServletResponse response) {
+       try {
+           // build the UML source from the compressed request parameter
+           
+           String uml = converterUtils.getUmlSource(src);
+           converterUtils.exportAsDiagramm(uml, response, FileFormat.UTXT);
+       } catch (IIOException iioe) {
+               // Browser has closed the connection, so the HTTP OutputStream is closed
+               // Silently catch the exception to avoid annoying log
+       } catch (Exception e) {
+           System.err.println("exception start for src:" + src + " ex:" + e);
+           e.printStackTrace();
+       }
+   }
+}
---
+++ src/main/java/de/yaio/services/plantuml/controller/PlantumlUtils.java
@@ -0,0 +1,120 @@
+/** 
+ * software for diagram-converter
+ * 
+ * @FeatureDomain                Converter
+ * @author                       Michael Schreiner <michael.schreiner@your-it-fellow.de>
+ * @category                     diagram-services
+ * @copyright                    Copyright (c) 2014, Michael Schreiner
+ * @license                      http://mozilla.org/MPL/2.0/ Mozilla Public License 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.yaio.services.plantuml.controller;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.SourceStringReader;
+import net.sourceforge.plantuml.StringUtils;
+import net.sourceforge.plantuml.code.Transcoder;
+import net.sourceforge.plantuml.code.TranscoderUtil;
+
+import org.springframework.stereotype.Service;
+
+/** 
+ * services to convert plantuml-src to diagrams
+ *  
+ * @FeatureDomain                service
+ * @package                      de.yaio.services.plantuml.controller
+ * @author                       Michael Schreiner <michael.schreiner@your-it-fellow.de>
+ * @category                     diagram-services
+ * @copyright                    Copyright (c) 2014, Michael Schreiner
+ * @license                      http://mozilla.org/MPL/2.0/ Mozilla Public License 2.0
+ */
+@Service
+class PlantumlUtils {
+    private static final Map<FileFormat, String> CONTENT_TYPE;
+    static {
+        Map<FileFormat, String> map = new HashMap<FileFormat, String>();
+        map.put(FileFormat.PNG, "image/png");
+        map.put(FileFormat.SVG, "image/svg+xml");
+        map.put(FileFormat.UTXT, "text/plain;charset=UTF-8");
+        CONTENT_TYPE = Collections.unmodifiableMap(map);
+    }
+
+    /**
+     * generate and export plantuml-diagram from uml
+     * @FeatureDomain                Webservice
+     * @FeatureResult                send diagram-data via HttpServletResponse
+     * @FeatureKeywords              Webservice
+     * @param uml                    the src of the plantuml-diagram
+     * @param response               the response to send the diagram
+     * @param format                 format of the diagram
+     * @throws IOException           possible Exception
+     */
+    public void exportAsDiagramm(String uml, HttpServletResponse response, FileFormat format) throws IOException {
+        if (StringUtils.isDiagramCacheable(uml)) {
+            addHeaderForCache(response);
+        }
+        response.setContentType(getContentType(format));
+        SourceStringReader reader = new SourceStringReader(uml);
+        reader.generateImage(response.getOutputStream(), new FileFormatOption(format, false));
+    }
+
+    protected void addHeaderForCache(HttpServletResponse response) {
+        long today = System.currentTimeMillis();
+        // Add http headers to force the browser to cache the image
+        response.addDateHeader("Expires", today + 31536000000L);
+        // today + 1 year
+        response.addDateHeader("Last-Modified", 1261440000000L);
+        // 2009 dec 22 constant date in the past
+        response.addHeader("Cache-Control", "public");
+    }
+
+    protected String getContentType(FileFormat format) {
+        return CONTENT_TYPE.get(format);
+    }
+
+    protected String getUmlSource(String source) {
+
+        // build the UML source from the compressed part of the URL
+        String text;
+        try {
+            text = URLDecoder.decode(source, "UTF-8");
+        } catch (UnsupportedEncodingException uee) {
+            text = "' invalid encoded string";
+        }
+        Transcoder transcoder = TranscoderUtil.getDefaultTranscoder();
+        try {
+            text = transcoder.decode(text);
+        } catch (IOException ioe) {
+            text = "' unable to decode string";
+        }
+
+        // encapsulate the UML syntax if necessary
+        String uml;
+        if (text.startsWith("@start")) {
+            uml = text;
+        } else {
+            StringBuilder plantUmlSource = new StringBuilder();
+            plantUmlSource.append("@startuml\n");
+            plantUmlSource.append(text);
+            if (text.endsWith("\n") == false) {
+                plantUmlSource.append("\n");
+            }
+            plantUmlSource.append("@enduml");
+            uml = plantUmlSource.toString();
+        }
+        return uml;
+    }
+}
